3a58f1e8303237f58a0fc5ab3d32624fb5fef253
GO Lee <golee.dev@gmail.com>
2020-12-29 22:32:15 +0900

    0.2 typescript basic

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Go", age = 1, gender = "female";
+const sayHi = (n, a, g) => {
+    console.log(`hi, ${n}, you are ${a} and a ${g}`);
+};
+sayHi(name, age, gender);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("hello");
+const name = "Go",
+    age = 1,
+    gender = "female";
+
+const sayHi = (n:string, a:number, g:string) => {
+    console.log(`hi, ${n}, you are ${a} and a ${g}`);
+}
+
+sayHi(name, age, gender);
+
+export { };
+
